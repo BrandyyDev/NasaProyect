12a398ea210a62fe680f4cb5ace96017ea64c2be
Brandon Villanueva <xbox-eddy2012@hotmail.com>
2025-05-05 14:49:13 -0600

    Node

--- nasaFrontend/src/components/organisms/paginador/paginadorStyled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const CardsContainerPaginator = styled.div`
-  display: grid;
-  gap: 1rem;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  padding: 1rem;
-`;
